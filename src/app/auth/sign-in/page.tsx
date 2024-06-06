7d2f3a62740bee272744e6db6ba0ca8eef69ffd1
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-06-05 21:36:27 -0500

    Implement 15 % of sign in component

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,4 +1,29 @@
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+
 export default function SignIn() {
-    return <div>SignIn View</div>;
-  }
-  
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="baseline"
+          sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+          <Typography variant="h3">Sign In</Typography>
+          <Typography
+            component={Link}
+            href={""}
+            variant="body1"
+            sx={{ textDecoration: "none" }}
+            color="primary"
+            passHref>
+              Dont have an account?
+            </Typography>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+}
