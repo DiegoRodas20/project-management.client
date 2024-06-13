5fb5d8428a40c03e0f95bbd488210535942bd711
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-06-12 21:02:04 -0500

    .

--- src/app/admin/layout/dashboard-layout.tsx
+++ src/app/admin/layout/dashboard-layout.tsx
@@ -1,16 +1,29 @@
 import { Box, Container, Toolbar } from '@mui/material';
 import { ReactNode } from "react";
 import Header from './header';
+import DrawerTest from './drawer-test';
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
 
     return (
         <Box sx={{ display: 'flex', width: '100%' }}>
-            <Header/>
-            <Box component="main">
-                <Toolbar></Toolbar>
-                <Container></Container>
-                {children}
+            <Header />
+            <DrawerTest />
+
+            <Box component="main" sx={{ width: `calc(100% - 280px)`, flexGrow: 1, p: { xs: 1, sm: 3 } }}>
+                <Toolbar sx={{ mt: 'horizontal', mb: 'horizontal' }}></Toolbar>
+                <Container
+                    maxWidth={'xl'}
+                    sx={{
+                        xs: 0,
+                        position: 'relative',
+                        minHeight: 'calc(100vh - 110px)',
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}>
+
+                    {children}
+                </Container>
             </Box>
         </Box>
     );
