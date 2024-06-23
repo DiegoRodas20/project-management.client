3de9cc231d803325e58b880b222e3d49af9f4d87
DiegoRodas20 <diegorodas2008@hotmail.com>
2024-06-23 12:42:00 -0500

    theme implemented

--- src/shared/theme/theme-customization.tsx
+++ src/shared/theme/theme-customization.tsx
@@ -1,8 +1,8 @@
 import CssBaseline from '@mui/material/CssBaseline';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 import { Theme, ThemeProvider } from '@mui/material/styles';
 import { ReactNode } from "react";
 import createThemeOptions from './create-theme-options';
-import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 
 export default function ThemeCustomization({ children }: { children: ReactNode }) {
 
